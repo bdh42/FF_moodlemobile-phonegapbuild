51478ca5aaf4b8ec9d02f061cb4aa0e77ed5a869
bdh42 <bdhale@gmail.com>
2017-01-10 12:04:39 -0500

    update to localhost to test css color changes

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -47699,7 +47699,7 @@ angular.module('mm.core')
     "demo_sites": {"student": {"url": "http://school.demo.moodle.net", "username": "student", "password": "moodle"}, "teacher": {"url": "http://school.demo.moodle.net", "username": "teacher", "password": "moodle"}, "cva": {"url": "http://mm.cvaconsulting.com/moodle", "username": "student", "password": "student"}},
     "gcmpn": "694767596569",
     "customurlscheme": "moodlemobile",
-    "siteurl": "http://famfirst.moonami.com",
+    "siteurl": "10.0.0.191",
     "skipssoconfirmation": "false",
     "forcedefaultlanguage": "false"
 }
